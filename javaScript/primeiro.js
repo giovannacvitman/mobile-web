b11b00ddc93534fc44dff5f16d6177decbb3de37
giovannacvitman <giovannacvitman@gmail.com>
2025-09-01 15:48:39 -0300

    Adicionando o primeiro código JavaScript

--- javaScript/primeiro.js
+++ javaScript/primeiro.js
@@ -3,3 +3,4 @@ let nome= "Giovanna";
 console.log(nome)
 console.log("Meu time é o Paulista")
 console.log("Eu tenho 18 anos")
+
---
+++ javaScript/variavel.js
@@ -0,0 +1,9 @@
+let mensagem = "olá mundo";
+const nomeCompleto = "Giovanna Vitman";
+
+let idade =18;
+
+//Resultado
+console.log(mensagem);
+console.log(nomeCompleto);
+console.log(nomeCompleto + " e minha idade é: " + idade);
