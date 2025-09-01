e5314ffebf65f78cccbfe829f3ef323859ca1c46
giovannacvitman <giovannacvitman@gmail.com>
2025-09-01 15:54:51 -0300

    Adicionando pastas

--- javaScript/variavel.js
+++ javaScript/variavel.js
@@ -6,4 +6,4 @@ let idade =18;
 //Resultado
 console.log(mensagem);
 console.log(nomeCompleto);
-console.log(nomeCompleto + " e minha idade é: " + idade);
+console.log("Meu nome é : " + nomeCompleto + " e minha idade é: " + idade + "Anos");
